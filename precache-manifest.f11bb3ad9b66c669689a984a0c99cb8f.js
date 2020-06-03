386271d84c06b19ccf0beb96e7dba037cfee0b4e
Lucas Ribeiro <lucas1995ls@outlook.com>
2020-06-02 22:21:11 -0300

    Updates

---
+++ precache-manifest.f11bb3ad9b66c669689a984a0c99cb8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4bd760b19278d11fc4ca0837d79318c",
+    "url": "/projetos/index.html"
+  },
+  {
+    "revision": "7731a0970ef2c223b14e",
+    "url": "/projetos/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c623d30628c24fefe589",
+    "url": "/projetos/static/js/2.ca0397b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projetos/static/js/2.ca0397b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7731a0970ef2c223b14e",
+    "url": "/projetos/static/js/main.7db57cfb.chunk.js"
+  },
+  {
+    "revision": "16a4c53477743fc85402",
+    "url": "/projetos/static/js/runtime-main.d5c41b91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/projetos/static/media/logo.5d5d9eef.svg"
+  }
+]);
